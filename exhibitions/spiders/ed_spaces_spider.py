741390ae4044136a18c72ea90d0f51a2d606a466
Daniel <37304160+daniktl@users.noreply.github.com>
2021-10-26 16:58:54 +0200

    Ed Spaces - adjusting headers (#12)
    * 🆕 - Ed Spaces spider

--- exhibitions/spiders/ed_spaces_spider.py
+++ exhibitions/spiders/ed_spaces_spider.py
@@ -15,14 +15,11 @@ class EdSpacesSpider(BaseA2ZSpider):
     ]
 
     HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
-        "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "macOS",
-        "DNT": 1,
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
         "Upgrade-Insecure-Requests": 1,
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "Host": "edspaces.a2zinc.net",
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive",
+        "Sec-GPC": 1,
     }
