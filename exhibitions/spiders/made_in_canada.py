e1ae758e5e6a8c1d1dfc66ba862f9b5596148e1b
Daniel Martich <danik4tl@gmail.com>
2021-11-09 22:23:23 +0100

    🆕 - black

--- exhibitions/spiders/made_in_canada.py
+++ exhibitions/spiders/made_in_canada.py
@@ -40,24 +40,36 @@ class MadeInCanadaSpider(BaseSpider):
                 headers=self.HEADERS,
             )
 
-        next_page: Optional[str] = response.xpath('//a[contains(@class, "next")]/@href').get()
+        next_page: Optional[str] = response.xpath(
+            '//a[contains(@class, "next")]/@href'
+        ).get()
         if next_page:
             yield response.follow(
-                url=next_page,
-                callback=self.fetch_exhibitors,
-                headers=self.HEADERS
+                url=next_page, callback=self.fetch_exhibitors, headers=self.HEADERS
             )
 
     def parse_exhibitors(self, response: Response):
         exhibitor_item = self.item_loader(self.item(), response)
-        exhibitor_name = response.xpath('//*[@class="entry-content"]/p[contains(.//text(), "Name")]/text()').get()
+        exhibitor_name = response.xpath(
+            '//*[@class="entry-content"]/p[contains(.//text(), "Name")]/text()'
+        ).get()
         # alternative path if the name was not preformatted right
         if not exhibitor_name:
-            exhibitor_name = response.xpath('//*[@class="entry-content"]/p[contains(.//text(), "Name")]/text()').get("")
+            exhibitor_name = response.xpath(
+                '//*[@class="entry-content"]/p[contains(.//text(), "Name")]/text()'
+            ).get("")
             exhibitor_name = exhibitor_name.split(":")[-1]
         exhibitor_item.add_value("exhibitor_name", exhibitor_name)
         exhibitor_item.add_xpath("exhibitor_name", '//h1[@class="entry-title"]/text()')
-        exhibitor_item.add_xpath("category", '//*[@class="entry-content"]/p[contains(.//text(), "Products")]/text()')
-        exhibitor_item.add_xpath("website", '//*[@class="entry-content"]/p[contains(.//text(), "Website")]/a/@href')
-        exhibitor_item.add_xpath("description", '//*[@class="entry-content"]/p[not(b) and not(strong)][1]')
+        exhibitor_item.add_xpath(
+            "category",
+            '//*[@class="entry-content"]/p[contains(.//text(), "Products")]/text()',
+        )
+        exhibitor_item.add_xpath(
+            "website",
+            '//*[@class="entry-content"]/p[contains(.//text(), "Website")]/a/@href',
+        )
+        exhibitor_item.add_xpath(
+            "description", '//*[@class="entry-content"]/p[not(b) and not(strong)][1]'
+        )
         yield exhibitor_item.load_item()
