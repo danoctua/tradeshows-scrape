8d0dd3e2d09846dea1163e6fd00f1aad36381083
Daniel Martich <danik4tl@gmail.com>
2021-11-16 09:23:15 +0100

    🆕 - Top Drawer spider

---
+++ exhibitions/item_loaders/top_drawer_item_loader.py
@@ -0,0 +1,25 @@
+import re
+from itemloaders.processors import Join
+
+from exhibitions.item_loaders.base_item_loaders.base_item_loader import (
+    BaseItemLoader,
+)
+
+
+BOOTH_NUMBER_REGEX = re.compile(r"Stand: (?P<booth_number>[^\r\n]*)")
+
+
+class TopDrawerItemLoader(BaseItemLoader):
+
+    description_out = Join(" ")
+
+    @staticmethod
+    def country_out(address_lines):
+        return address_lines[-1]
+
+    @staticmethod
+    def booth_number_in(booth_number_lines):
+        for booth_number_line in booth_number_lines:
+            booth_number_search = BOOTH_NUMBER_REGEX.search(booth_number_line)
+            if booth_number_search:
+                yield booth_number_search.group("booth_number")
