6153c9bfc2c973cf9a6f3729a295c5b0bff389d0
Daniel <37304160+daniktl@users.noreply.github.com>
2021-11-29 22:42:58 +0100

    🆕 - Christmas World spider (#32)

---
+++ exhibitions/spiders/christmas_world_spider.py
@@ -0,0 +1,17 @@
+import datetime
+from exhibitions.spiders.base_spiders.base_messe_frankfurt_spider import (
+    BaseMesseFrankfurtSpider,
+)
+
+
+class ChristmasWorldSpider(BaseMesseFrankfurtSpider):
+    name = "ChristmasWorldSpider"
+
+    EXHIBITION_DATE = datetime.date(2022, 1, 28)
+    EXHIBITION_NAME = "Ambiente"
+    EXHIBITION_WEBSITE = "https://christmasworld.messefrankfurt.com"
+
+    FIND_EVENT_VARIABLE: str = "CHRISTMASWORLD"
+    EXHIBITOR_SEARCH_URL: str = (
+        "https://exhibitorsearch.messefrankfurt.com/service/esb/2.1/search/exhibitor"
+    )
