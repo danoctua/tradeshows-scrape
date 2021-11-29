7627defb7f8a4c8bf408cdb4b8629e9c6dbf005b
Daniel Martich <danik4tl@gmail.com>
2021-11-29 22:28:10 +0100

    🆕 - Surfaces spider

---
+++ exhibitions/spiders/surfaces_spider.py
@@ -0,0 +1,13 @@
+import datetime
+
+from exhibitions.spiders.base_spiders.base_map_your_show_spider import BaseMapYourShowSpider
+
+
+class SurfacesSpider(BaseMapYourShowSpider):
+    name = "SurfacesSpider"
+
+    EXHIBITION_DATE = datetime.date(2022, 2, 1)
+    EXHIBITION_NAME = "Surfaces 2022"
+    EXHIBITION_WEBSITE = "https://www.intlsurfaceevent.com/"
+
+    EXHIBITION_CODE = "sur221"
