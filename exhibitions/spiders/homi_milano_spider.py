bbcfebd7d121a25098a735056dc961bd1e1053c4
Daniel Martich <danik4tl@gmail.com>
2021-12-01 11:52:48 +0100

    🆕 - Homi trying to avoid blocking

--- exhibitions/spiders/homi_milano_spider.py
+++ exhibitions/spiders/homi_milano_spider.py
@@ -20,11 +20,17 @@ class HomiMilanoSpider(BaseSpider):
 
     HEADERS = {
         "Accept": "*/*",
+        "Accept-Encoding": "zip, deflate, br",
         "X-Requested-With": "XMLHttpRequest",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Sec-GPC": 1,
         "Host": "expoplaza-homi.fieramilano.it",
+        "Origin": "https://expoplaza-homi.fieramilano.it",
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
     }
 
     item_loader = BaseItemLoader
@@ -34,20 +40,34 @@ class HomiMilanoSpider(BaseSpider):
     LIST_EXHIBITORS_URL: str = "https://expoplaza-homi.fieramilano.it/en/search/exhibitors"
     REQUEST_TOKEN_REGEX = re.compile(r"'requestObject': searchObject, 'token': '(?P<request_token>.*)'")
     token: str
+    cookies: str
 
     URLS = [GET_EXHIBITORS_INITIAL_URL]
 
     PROXY = PROXY_ZYTE
 
     custom_settings = {
-        # "DOWNLOADER_MIDDLEWARES": {
-        #     "exhibitions.middlewares.proxy_session_middleware.ProxySessionDownloaderMiddleware": 0,
-        # },
+        "DOWNLOADER_MIDDLEWARES": {
+            "exhibitions.middlewares.proxy_middleware.ProxyDownloaderMiddleware": 0,
+            "scrapy.downloadermiddlewares.cookies.CookiesMiddleware": 5
+        },
         "ITEM_PIPELINES": {
             "exhibitions.pipelines.prefetch_exhibition_data_pipeline.PrefetchExhibitionDataPipeline": 10,
             "exhibitions.pipelines.export_item_pipeline.ExportItemPipeline": 100,
-        }
+        },
+        "COOKIES_ENABLED": True,
+        "COOKIES_DEBUG": True
     }
+
+    COOKIE_JAR = "cookiejar"
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.GET_EXHIBITORS_INITIAL_URL,
+            headers=self.HEADERS,
+            meta={"cookiejar": self.COOKIE_JAR},
+            callback=self.fetch_exhibitors
+        )
 
     def _get_request_body(self, offset: int = 1) -> dict:
         return {
@@ -69,16 +89,17 @@ class HomiMilanoSpider(BaseSpider):
             yield response.follow(
                 url=f"{exhibitor}/info",
                 callback=self.parse_exhibitors,
-                meta={**response.meta, "offset": response.meta.get("offset", )}
+                meta=response.meta,
+                headers=self.HEADERS,
+                cookies=response.request.cookies
             )
         current_offset = response.meta.get("offset", 0)
-        print("CURRENT OFFSET", current_offset)
         yield scrapy.FormRequest(
             url=self.LIST_EXHIBITORS_URL,
             formdata=self._get_request_body(offset=current_offset + 1),
             meta={**response.meta, "offset": current_offset + 1},
             callback=self.fetch_additional_exhibitors,
-            headers=self.HEADERS
+            headers=self.HEADERS,
         )
 
     def parse_exhibitors(self, response: TextResponse):
