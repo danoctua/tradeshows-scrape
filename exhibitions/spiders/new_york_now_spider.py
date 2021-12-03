2bd6b3f7d1f5707889257c43e2534f24a1884841
Daniel <37304160+daniktl@users.noreply.github.com>
2021-12-03 15:34:57 +0100

    🆕 - New York Now spider (#40)

---
+++ exhibitions/spiders/new_york_now_spider.py
@@ -0,0 +1,38 @@
+import datetime
+
+from exhibitions.constants import PROXY_ZYTE
+from exhibitions.spiders.base_spiders.base_a2z_spider import BaseA2ZSpider
+
+
+class NewYorkNowSpider(BaseA2ZSpider):
+    name = "NewYorkNowSpider"
+
+    EXHIBITION_DATE = datetime.date(2022, 2, 6)
+    EXHIBITION_NAME = "New York Now Winter"
+    EXHIBITION_WEBSITE = "https://nynow.com/future-market/"
+
+    URLS = [
+        "https://nynow.a2zinc.net/Winter2022/Public/exhibitors.aspx?Index=All",
+    ]
+
+    PROXY = PROXY_ZYTE
+
+    custom_settings = {
+        "ITEM_PIPELINES": {
+            "exhibitions.pipelines.prefetch_exhibition_data_pipeline.PrefetchExhibitionDataPipeline": 10,
+            "exhibitions.pipelines.export_item_pipeline.ExportItemPipeline": 100,
+        },
+        "DOWNLOADER_MIDDLEWARES": {
+            "exhibitions.middlewares.proxy_middleware.ProxyDownloaderMiddleware": 0,
+        },
+    }
+
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
+        "Upgrade-Insecure-Requests": 1,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Host": "nynow.a2zinc.net",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Sec-GPC": 1,
+    }
